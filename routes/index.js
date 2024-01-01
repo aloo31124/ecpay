e935d995bd3c7efb6e55522b46ae8676301b067d
aloo31124 <aloo31124@gmail.com>
2024-01-01 11:55:15 +0800

    feat [CheckMacValue] 雜湊SHA256 固定-方便測試

--- routes/index.js
+++ routes/index.js
@@ -6,51 +6,38 @@ require('dotenv').config();
 // 綠界提供的 SDK
 const ecpay_payment = require('ecpay_aio_nodejs');
 
-const { MERCHANTID, HASHKEY, HASHIV, HOST } = process.env;
+//const { MERCHANTID, HASHKEY, HASHIV, HOST } = process.env;
 
 // SDK 提供的範例，初始化
 // https://github.com/ECPay/ECPayAIO_Node.js/blob/master/ECPAY_Payment_node_js/conf/config-example.js
+// A594B2931614CC62AB40AAD86399284DB7AC2902BC5B6AAEB8BFF22C0B91F93B
 const options = {
   OperationMode: 'Test', //Test or Production
   MercProfile: {
-    MerchantID: MERCHANTID,
-    HashKey: HASHKEY,
-    HashIV: HASHIV,
+    MerchantID: "2000132",
+    HashKey: "5294y06JbISpM5x9",
+    HashIV: "v77hoKGq4kWxNNIS",
   },
-  IgnorePayment: [
-    //    "Credit",
-    //    "WebATM",
-    //    "ATM",
-    //    "CVS",
-    //    "BARCODE",
-    //    "AndroidPay"
-  ],
+  IgnorePayment: [],
   IsProjectContractor: false,
 };
 let TradeNo;
 
 router.get('/', (req, res) => {
+  console.log(" ec pay start!");
   // SDK 提供的範例，參數設定
   // https://github.com/ECPay/ECPayAIO_Node.js/blob/master/ECPAY_Payment_node_js/conf/config-example.js
-  const MerchantTradeDate = new Date().toLocaleString('zh-TW', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false,
-    timeZone: 'UTC',
-  });
-  TradeNo = 'test' + new Date().getTime();
+
+  
+  MerchantTradeDate = '2023/03/12 15:30:23' //日期固定
   let base_param = {
-    MerchantTradeNo: TradeNo, //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
+    MerchantTradeNo: 'f0a0d7e9fae1bb72bc92', //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
     MerchantTradeDate,
     TotalAmount: '87',
     TradeDesc: '測試交易描述',
     ItemName: '測試商品等',
-    ReturnURL: `${HOST}/return`,
-    ClientBackURL: `${HOST}/clientReturn`,
+    ReturnURL: "/return",
+    ClientBackURL: "/clientReturn",
   };
   const create = new ecpay_payment(options);
 
@@ -62,6 +49,9 @@ router.get('/', (req, res) => {
     title: 'loulou test Express',
     html,
   });
+
+  
+  console.log(" ec pay end !");
 });
 
 // 後端接收綠界回傳的資料
